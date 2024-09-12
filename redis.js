321c2d9bf2a2927682216502f57e37cd2ad89fe2
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-09-13 01:52:21 +1200

    move redis url to env file

--- redis.js
+++ redis.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const { createClient } = require('redis');
 const RedisStore = require('connect-redis').default;
 
 const redisClient = createClient({
-  url: 'redis://localhost:6379', // Use your Redis server's URL and port
+  url: process.env.REDIS_URL,
 });
 
 const sessionStore = new RedisStore({ client: redisClient });
